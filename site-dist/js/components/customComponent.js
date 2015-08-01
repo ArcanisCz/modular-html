429a202b59b1acc6a36fa6cd79a93e22a8012842
Arcanis <arcaniscz@gmail.com>
2015-08-01 16:31:36 +0200

    multiple imports

--- site-dist/js/components/customComponent.js
+++ site-dist/js/components/customComponent.js
@@ -1,15 +1,19 @@
 define([
     "jquery"
 ], function ($) {
-    return function(parentElement, selector, templateSelector, initFunction){
-        parentElement = $(parentElement)
-        var templates = document.querySelector('link[rel="import"]').import;
-        var template = templates.querySelector(templateSelector);
-        parentElement.find(selector).append(template.content.cloneNode(true));
-        var element = parentElement.find(selector)[0];
-        if(initFunction){
-            initFunction.call(element, element);
+    return function (parentElement, selector, templateSelector, initFunction) {
+        parentElement = $(parentElement);
+        var templates = document.querySelectorAll('link[rel="import"]');
+        for (var i= 0; i < templates.length; i++) {
+            var template = templates[i].import.querySelector(templateSelector);
+            if (template != null) {
+                parentElement.find(selector).append(template.content.cloneNode(true));
+                var element = parentElement.find(selector)[0];
+                if (initFunction) {
+                    initFunction.call(element, element);
+                }
+                return element;
+            }
         }
-        return element;
     };
 });
